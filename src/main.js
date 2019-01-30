91ddcbb79e8a70f058c1f580e27c152818de91c1
Serge Sozonoff <serge@sozonoff.com>
2019-01-30 20:37:17 +0200

    Vue js Muuri wrapper

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import MuuriGrid from './components/MuuriGrid.vue'
+import ItemLink from './components/items/ItemLink.vue'
+import ItemSm from './components/items/ItemSm.vue'
+import ItemMd from './components/items/ItemMd.vue'
+import ItemLg from './components/items/ItemLg.vue'
 
-Vue.config.productionTip = false
+var plugin = {
+    install: function (Vue, store) {
+        Vue.component('muuri-grid', MuuriGrid);
+        Vue.component('item-sm', ItemSm);
+        Vue.component('item-md', ItemMd);
+        Vue.component('item-lg', ItemLg);
+        Vue.component('item-ln', ItemLink);
+        Vue.component('version', version);
 
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+    }
+};
+
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__';
+
